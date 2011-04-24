ded54198e9960844d3c9ae180836b86cd6a8cd97
Evan K. Friis <ekfriis@gmail.com>
2011-04-24 15:39:54 +0200

    Updating pub lists, improving typesetting

--- dump_spires.sh
+++ dump_spires.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 cat ~/Library/texmf/bibtex/bib/spires_papers.bib | grep Article | sed \
-"s|^@[Aa]rticle{||"  | sed "s|,$||" 
+"s|^@[Aa]rticle{||"  | sed "s|,$||"  | sed 's|.*|\\bibentry{&}\\\\ ~\\\\|'
 
